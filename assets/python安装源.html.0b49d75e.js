6df1ab455f27f7941de5370e0ce7d997b6f89296
xlc520 <56480807+xlc520@users.noreply.github.com>
2022-06-07 05:48:30 +0000

    Deploying to gh-pages from @ xlc520/xlc520.github.io@97615a4edb9ac2afbac2809ddc195bb9828cd042 🚀

---
+++ assets/python安装源.html.0b49d75e.js
@@ -0,0 +1,23 @@
+import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";import{o as s,c as a,d as e}from"./app.d7745028.js";const p={},o=e(`<h1 id="python\u5B89\u88C5\u6E90" tabindex="-1"><a class="header-anchor" href="#python\u5B89\u88C5\u6E90" aria-hidden="true">#</a> python\u5B89\u88C5\u6E90</h1><p>\u5728<code>C:\\Users\\Administrator\\pip</code>\u4E0B\uFF0C\u65B0\u5EFA<code>pip.ini</code>\uFF0C\u8F93\u5165\u4EE5\u4E0B\u5185\u5BB9\uFF1A</p><div class="language-python ext-py line-numbers-mode"><pre class="language-python"><code><span class="token punctuation">[</span><span class="token keyword">global</span><span class="token punctuation">]</span>
+timeout <span class="token operator">=</span> <span class="token number">6000</span>
+index<span class="token operator">-</span>url <span class="token operator">=</span> https<span class="token punctuation">:</span><span class="token operator">//</span>mirrors<span class="token punctuation">.</span>aliyun<span class="token punctuation">.</span>com<span class="token operator">/</span>pypi<span class="token operator">/</span>simple<span class="token operator">/</span>
+<span class="token punctuation">[</span>install<span class="token punctuation">]</span>
+use<span class="token operator">-</span>mirrors <span class="token operator">=</span> true
+mirrors <span class="token operator">=</span> https<span class="token punctuation">:</span><span class="token operator">//</span>mirrors<span class="token punctuation">.</span>aliyun<span class="token punctuation">.</span>com<span class="token operator">/</span>pypi<span class="token operator">/</span>simple<span class="token operator">/</span>
+trusted<span class="token operator">-</span>host <span class="token operator">=</span> mirrors<span class="token punctuation">.</span>aliyun<span class="token punctuation">.</span>com
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>\u5176\u4ED6\uFF1A</p><div class="language-python ext-py line-numbers-mode"><pre class="language-python"><code><span class="token punctuation">[</span><span class="token keyword">global</span><span class="token punctuation">]</span>
+<span class="token comment"># \u963F\u91CC\u4E91</span>
+index<span class="token operator">-</span>url <span class="token operator">=</span> https<span class="token punctuation">:</span><span class="token operator">//</span>mirrors<span class="token punctuation">.</span>aliyun<span class="token punctuation">.</span>com<span class="token operator">/</span>pypi<span class="token operator">/</span>simple<span class="token operator">/</span> 
+<span class="token comment"># \u6E05\u534E\u5927\u5B66</span>
+<span class="token comment"># index-url = https://pypi.tuna.tsinghua.edu.cn/simple/</span>
+<span class="token comment"># \u4E2D\u56FD\u79D1\u6280\u5927\u5B66</span>
+<span class="token comment"># index-url = https://pypi.mirrors.ustc.edu.cn/simple/ </span>
+<span class="token comment"># \u8C46\u74E3</span>
+<span class="token comment"># index-url = http://pypi.douban.com/simple </span>
+<span class="token comment"># Python\u5B98\u65B9</span>
+<span class="token comment"># index-url = https://pypi.python.org/simple/ </span>
+<span class="token comment"># v2ex</span>
+<span class="token comment"># index-url = http://pypi.v2ex.com/simple/ </span>
+<span class="token comment"># \u4E2D\u56FD\u79D1\u5B66\u9662</span>
+<span class="token comment"># index-url = http://pypi.mirrors.opencas.cn/simple/ </span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),t=[o];function i(c,l){return s(),a("div",null,t)}var d=n(p,[["render",i],["__file","python\u5B89\u88C5\u6E90.html.vue"]]);export{d as default};
