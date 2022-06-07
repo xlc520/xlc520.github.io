97615a4edb9ac2afbac2809ddc195bb9828cd042
xlc520 <2215400217@qq.com>
2022-06-07 13:45:14 +0800

    update lib

--- notes/.vuepress/config.ts
+++ notes/.vuepress/config.ts
@@ -10,6 +10,26 @@ export default defineUserConfig({
   base: "/",
 
   head: [
+    // 百度统计
+    [
+      "script",{},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?94d325c8ef98034a16f401082b6295a0";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+      `
+    ],
+    // 51.la统计
+    [
+      "script",{},
+      `
+      !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JiDO506kAypiF3n8",ck:"JiDO506kAypiF3n8",autoTrack:true,hashMode:true});
+      `
+    ],
     // 解决403错误
     ["meta", { name: "referrer", content: "no-referrer" }],
     [
