e51fc35f2247a6477b58f297c2bb123ecd8a8485
xlc520 <2215400217@qq.com>
2022-07-08 17:33:36 +0800

    添加回icon

--- notes/.vuepress/config.ts
+++ notes/.vuepress/config.ts
@@ -8,37 +8,37 @@ export default defineUserConfig({
   title: "StudyNote",
   description: "个人学习的笔记，记录学习过程遇到的问题，学到的知识，收集各种学习工具，各种技巧，各种使用教程。",
   base: "/",
-  // head: [
-  //   // 百度统计
-  //   [
-  //     "script",{},
-  //     `
-  //     var _hmt = _hmt || [];
-  //     (function() {
-  //       var hm = document.createElement("script");
-  //       hm.src = "https://hm.baidu.com/hm.js?94d325c8ef98034a16f401082b6295a0";
-  //       var s = document.getElementsByTagName("script")[0]; 
-  //       s.parentNode.insertBefore(hm, s);
-  //     })();
-  //     `
-  //   ],
-  //   // 51.la统计
-  //   [
-  //     "script",{},
-  //     `
-  //     !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JiDO506kAypiF3n8",ck:"JiDO506kAypiF3n8",autoTrack:true,hashMode:true});
-  //     `
-  //   ],
-  //   // 解决403错误
-  //   ["meta", { name: "referrer", content: "no-referrer" }],
-  //   [
-  //     "link",
-  //     {
-  //       rel: "stylesheet",
-  //       href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
-  //     },
-  //   ],
-  // ],
+  head: [
+    // 百度统计
+    [
+      "script",{},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?94d325c8ef98034a16f401082b6295a0";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+      `
+    ],
+    // 51.la统计
+    [
+      "script",{},
+      `
+      !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JiDO506kAypiF3n8",ck:"JiDO506kAypiF3n8",autoTrack:true,hashMode:true});
+      `
+    ],
+    // 解决403错误
+    ["meta", { name: "referrer", content: "no-referrer" }],
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
+      },
+    ],
+  ],
   locales: {
     "/": {
       lang: "zh-CN",
