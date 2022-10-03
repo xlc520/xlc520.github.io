2e08c69f7e5778f5ed261dc49ea36eaf9442c837
xlc520 <56480807+xlc520@users.noreply.github.com>
2022-10-03 16:18:00 +0000

    Deploying to gh-pages from @ xlc520/xlc520.github.io@b95592b69e9c530e97a891bed6a01713436ed9ac 🚀

---
+++ assets/repository.html.ef5cb3fd.js
@@ -0,0 +1,49 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o,c as i,a as s,b as n,e as a,d as p,r}from"./app.ffebafef.js";const u={},l=s("h1",{id:"\u4EAC\u4E1C\u811A\u672C\u4ED3\u5E93",tabindex:"-1"},[s("a",{class:"header-anchor",href:"#\u4EAC\u4E1C\u811A\u672C\u4ED3\u5E93","aria-hidden":"true"},"#"),n(" \u4EAC\u4E1C\u811A\u672C\u4ED3\u5E93")],-1),c={href:"https://github.com/danteswx/fxxk",target:"_blank",rel:"noopener noreferrer"},d={href:"https://github.com/woaim65/Oz",target:"_blank",rel:"noopener noreferrer"},m={href:"https://github.com/xdhgsq/xdh",target:"_blank",rel:"noopener noreferrer"},b={href:"https://github.com/whzsyx/xuescripts",target:"_blank",rel:"noopener noreferrer"},g={href:"https://github.com/sanbing666/scripts",target:"_blank",rel:"noopener noreferrer"},k={href:"https://github.com/XCould/jd-scripts-docker",target:"_blank",rel:"noopener noreferrer"},h={href:"https://github.com/skyyaman/temp",target:"_blank",rel:"noopener noreferrer"},q={href:"https://github.com/linmudaye/linmudaye",target:"_blank",rel:"noopener noreferrer"},v=p(`<div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>
+chiupam\u4ED3\u5E93<span class="token punctuation">(</span>\u4EAC\u559C\u5DE5\u5382\u74DC\u5206\u7535\u529B\u5F00\u56E2ID<span class="token punctuation">)</span> ql repo https://github.com/chiupam/JD_Diy.git <span class="token string">&quot;activeId&quot;</span> <span class="token number">1</span> <span class="token number">1</span>-23/4 * * *
+\u67E0\u6AAC\uFF08\u80D6\u864E\uFF09\u4ED3\u5E93  ql repo https://github.com/panghu999/panghu.git <span class="token string">&quot;jd_&quot;</span>  <span class="token number">3</span> <span class="token number">1</span>-23/4 * * *
+
+Ariszy\uFF08Zhiyi-N\uFF09\u4ED3\u5E93  ql repo https://github.com/Ariszy/Private-Script.git <span class="token string">&quot;JD&quot;</span>  <span class="token number">5</span> <span class="token number">1</span>-23/4 * * *
+
+jiulan\u4ED3\u5E93  ql repo https://github.com/jiulan/platypus.git <span class="token string">&quot;jd_|jx_&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;overdue&quot;</span> <span class="token string">&quot;main&quot;</span> <span class="token number">7</span> <span class="token number">1</span>-23/4 * * *
+
+passerby-b\u4ED3\u5E93  ql repo https://github.com/passerby-b/JDDJ.git <span class="token string">&quot;jddj_&quot;</span> <span class="token string">&quot;_cookie|_getck&quot;</span> <span class="token string">&quot;^jd[^_]&quot;</span>  <span class="token number">9</span> <span class="token number">1</span>-23/4 * * *
+
+curtinlv\u4ED3\u5E93  ql repo https://github.com/curtinlv/JD-Script.git <span class="token string">&quot;jd_&quot;</span>  <span class="token number">13</span> <span class="token number">1</span>-23/4 * * *
+
+cdle\u4ED3\u5E93  ql repo https://github.com/cdle/xdd.git <span class="token string">&quot;jd_&quot;</span> <span class="token string">&quot;disposable|expired|jdc&quot;</span>  <span class="token number">15</span> <span class="token number">1</span>-23/4 * * *
+
+smiek2221<span class="token punctuation">(</span>\u9752\u86D9<span class="token punctuation">)</span>\u4ED3\u5E93  ql repo https://github.com/smiek2221/scripts.git <span class="token string">&quot;jd_|gua_&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;^MovementFaker|^JDJRValidator|^ZooFaker|^sign&quot;</span> <span class="token number">17</span> <span class="token number">1</span>-23/4 * * *
+
+star261\u4ED3\u5E93 ql repo https://github.com/star261/jd.git <span class="token string">&quot;jd_|star_&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;code&quot;</span> <span class="token string">&quot;main&quot;</span> <span class="token number">19</span> <span class="token number">1</span>-23/4 * * *
+
+\u6E29\u67D0\u4EBA\u4ED3\u5E93  ql repo https://github.com/Wenmoux/scripts.git <span class="token string">&quot;other|jd&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;wen&quot;</span> <span class="token number">21</span> <span class="token number">1</span>-23/4 * * *
+
+he1pu\u4ED3\u5E93  ql repo https://github.com/he1pu/JDHelp.git <span class="token string">&quot;jd_|jx_|getJDCookie&quot;</span> <span class="token string">&quot;Coupon|update&quot;</span> <span class="token string">&quot;^jd[^_]|USER|^sign|^ZooFaker|utils&quot;</span> <span class="token number">23</span> <span class="token number">1</span>-23/4 * * *
+
+shufflewzc\u4ED3\u5E93 ql repo https://github.com/shufflewzc/faker2.git <span class="token string">&quot;jd_|jx_|gua_|jddj_|getJDCookie&quot;</span> <span class="token string">&quot;activity|backUp|Coupon|update&quot;</span> <span class="token string">&quot;^jd[^_]|USER|^JS|^TS|^JDJRValidator_Pure|^ZooFaker|^sign&quot;</span> <span class="token number">25</span> <span class="token number">1</span>-23/4 * * *
+
+JDHelloWorld\u4ED3\u5E93 ql repo https://github.com/JDHelloWorld/jd_scripts.git <span class="token string">&quot;jd_|jx_|getJDCookie&quot;</span> <span class="token string">&quot;activity|backUp|Coupon|enen|update|test&quot;</span> <span class="token string">&quot;^jd[^_]|USER|^TS|utils|notify|env|package|ken.js&quot;</span> <span class="token number">27</span> <span class="token number">1</span>-23/4 * * *
+
+NobyDa\u4ED3\u5E93<span class="token punctuation">(</span>\u62C9\u53D6\u4EAC\u4E1C\u7B7E\u5230\u57FA\u7840\u811A\u672CJD_DailyBonus.js\u4E13\u7528<span class="token punctuation">)</span> ql repo https://github.com/NobyDa/Script.git <span class="token string">&quot;JD-DailyBonus&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;JD_DailyBonus&quot;</span> <span class="token string">&quot;master&quot;</span> <span class="token number">29</span> <span class="token number">1</span>-23/4 * * *
+
+Tsukasa007\u4ED3\u5E93 ql repo https://github.com/Tsukasa007/my_script.git <span class="token string">&quot;jd_|jx_&quot;</span> <span class="token string">&quot;jdCookie|USER_AGENTS|sendNotify|backup&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;master&quot;</span> <span class="token number">31</span> <span class="token number">1</span>-23/4 * * *
+
+Aaron-lv\u4ED3\u5E93 ql repo https://github.com/Aaron-lv/sync.git <span class="token string">&quot;jd_|jx_|getJDCookie&quot;</span> <span class="token string">&quot;activity|backUp|Coupon&quot;</span> <span class="token string">&quot;^jd[^_]|USER|utils&quot;</span> <span class="token string">&quot;jd_scripts&quot;</span> <span class="token number">33</span> <span class="token number">1</span>-23/4 * * *
+
+
+
+
+
+\u3010Faker\u96C6\u5408\u4ED3\u5E93\u3011
+ ql repo https://ghproxy.com/https://github.com/shufflewzc/faker2.git <span class="token string">&quot;jd_|jx_|gua_|jddj_|getJDCookie&quot;</span> <span class="token string">&quot;activity|backUp&quot;</span> <span class="token string">&quot;^jd[^_]|USER|ZooFaker_Necklace.js|JDJRValidator_Pure|sign_graphics_validate&quot;</span>
+
+ \u3010curtinlv\u4ED3\u5E93\u3011
+ql repo https://github.com/curtinlv/JD-Script.git
+
+\u3010\u6E29\u67D0\u67D0\u3011
+ql repo https://ghproxy.com/https://github.com/shufflewzc/Wenmoux.git 
+ 
+\u3010\u67E0\u6AAC\uFF08\u80D6\u864E\uFF09\u3011
+ql repo https://github.com/panghu999/panghu.git <span class="token string">&quot;jd_&quot;</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1);function _(j,f){const t=r("ExternalLinkIcon");return o(),i("div",null,[l,s("p",null,[n("2022\u5E742\u67088\u65E5 "),s("a",c,[n("https://github.com/danteswx/fxxk"),a(t)])]),s("p",null,[s("a",d,[n("https://github.com/woaim65/Oz"),a(t)])]),s("p",null,[s("a",m,[n("https://github.com/xdhgsq/xdh"),a(t)])]),s("p",null,[s("a",b,[n("https://github.com/whzsyx/xuescripts"),a(t)])]),s("p",null,[s("a",g,[n("https://github.com/sanbing666/scripts"),a(t)])]),s("p",null,[s("a",k,[n("https://github.com/XCould/jd-scripts-docker"),a(t)])]),s("p",null,[s("a",h,[n("https://github.com/skyyaman/temp"),a(t)])]),s("p",null,[s("a",q,[n("https://github.com/linmudaye/linmudaye"),a(t)])]),v])}const D=e(u,[["render",_],["__file","repository.html.vue"]]);export{D as default};
