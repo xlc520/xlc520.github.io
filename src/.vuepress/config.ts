12c1f93e6cb8a63cfd55759234309b09bbd10a4a
xlc520 <2215400217@qq.com>
2023-01-05 10:03:30 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,7 +4,7 @@ import { searchProPlugin } from "vuepress-plugin-search-pro";
 export default defineUserConfig({
   base: "/",
   lang: "zh-CN",
-  dest: "./blog",
+  dest: "./dist",
   title: "StudyNote",
   description: "个人学习的笔记，记录学习过程遇到的问题，学到的知识，收集各种学习工具，各种技巧，各种使用教程。",
   theme,
