1f07ce1edd910ed3977a2596d252c551847aa1f8
xlc520 <56480807+xlc520@users.noreply.github.com>
2024-02-05 11:28:40 +0000

    Deploying to gh-pages from @ xlc520/xlc520.github.io@61903ba4f43380a0f195aafe455955d30a3ef94d 🚀

---
+++ assets/repository.html-m9iog4Em.js
@@ -0,0 +1,49 @@
+import{_ as s}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as t,a}from"./app-AGOZjyqV.js";const e={},i=a(`<h1 id="京东脚本仓库" tabindex="-1"><a class="header-anchor" href="#京东脚本仓库"><span>京东脚本仓库</span></a></h1><p>2022年2月8日 https://github.com/danteswx/fxxk</p><p>https://github.com/woaim65/Oz</p><p>https://github.com/xdhgsq/xdh</p><p>https://github.com/whzsyx/xuescripts</p><p>https://github.com/sanbing666/scripts</p><p>https://github.com/XCould/jd-scripts-docker</p><p>https://github.com/skyyaman/temp</p><p>https://github.com/linmudaye/linmudaye</p><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code>
+chiupam仓库<span class="token punctuation">(</span>京喜工厂瓜分电力开团ID<span class="token punctuation">)</span> ql repo https://github.com/chiupam/JD_Diy.git <span class="token string">&quot;activeId&quot;</span> <span class="token number">1</span> <span class="token number">1</span>-23/4 * * *
+柠檬（胖虎）仓库  ql repo https://github.com/panghu999/panghu.git <span class="token string">&quot;jd_&quot;</span>  <span class="token number">3</span> <span class="token number">1</span>-23/4 * * *
+
+Ariszy（Zhiyi-N）仓库  ql repo https://github.com/Ariszy/Private-Script.git <span class="token string">&quot;JD&quot;</span>  <span class="token number">5</span> <span class="token number">1</span>-23/4 * * *
+
+jiulan仓库  ql repo https://github.com/jiulan/platypus.git <span class="token string">&quot;jd_|jx_&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;overdue&quot;</span> <span class="token string">&quot;main&quot;</span> <span class="token number">7</span> <span class="token number">1</span>-23/4 * * *
+
+passerby-b仓库  ql repo https://github.com/passerby-b/JDDJ.git <span class="token string">&quot;jddj_&quot;</span> <span class="token string">&quot;_cookie|_getck&quot;</span> <span class="token string">&quot;^jd[^_]&quot;</span>  <span class="token number">9</span> <span class="token number">1</span>-23/4 * * *
+
+curtinlv仓库  ql repo https://github.com/curtinlv/JD-Script.git <span class="token string">&quot;jd_&quot;</span>  <span class="token number">13</span> <span class="token number">1</span>-23/4 * * *
+
+cdle仓库  ql repo https://github.com/cdle/xdd.git <span class="token string">&quot;jd_&quot;</span> <span class="token string">&quot;disposable|expired|jdc&quot;</span>  <span class="token number">15</span> <span class="token number">1</span>-23/4 * * *
+
+smiek2221<span class="token punctuation">(</span>青蛙<span class="token punctuation">)</span>仓库  ql repo https://github.com/smiek2221/scripts.git <span class="token string">&quot;jd_|gua_&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;^MovementFaker|^JDJRValidator|^ZooFaker|^sign&quot;</span> <span class="token number">17</span> <span class="token number">1</span>-23/4 * * *
+
+star261仓库 ql repo https://github.com/star261/jd.git <span class="token string">&quot;jd_|star_&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;code&quot;</span> <span class="token string">&quot;main&quot;</span> <span class="token number">19</span> <span class="token number">1</span>-23/4 * * *
+
+温某人仓库  ql repo https://github.com/Wenmoux/scripts.git <span class="token string">&quot;other|jd&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;wen&quot;</span> <span class="token number">21</span> <span class="token number">1</span>-23/4 * * *
+
+he1pu仓库  ql repo https://github.com/he1pu/JDHelp.git <span class="token string">&quot;jd_|jx_|getJDCookie&quot;</span> <span class="token string">&quot;Coupon|update&quot;</span> <span class="token string">&quot;^jd[^_]|USER|^sign|^ZooFaker|utils&quot;</span> <span class="token number">23</span> <span class="token number">1</span>-23/4 * * *
+
+shufflewzc仓库 ql repo https://github.com/shufflewzc/faker2.git <span class="token string">&quot;jd_|jx_|gua_|jddj_|getJDCookie&quot;</span> <span class="token string">&quot;activity|backUp|Coupon|update&quot;</span> <span class="token string">&quot;^jd[^_]|USER|^JS|^TS|^JDJRValidator_Pure|^ZooFaker|^sign&quot;</span> <span class="token number">25</span> <span class="token number">1</span>-23/4 * * *
+
+JDHelloWorld仓库 ql repo https://github.com/JDHelloWorld/jd_scripts.git <span class="token string">&quot;jd_|jx_|getJDCookie&quot;</span> <span class="token string">&quot;activity|backUp|Coupon|enen|update|test&quot;</span> <span class="token string">&quot;^jd[^_]|USER|^TS|utils|notify|env|package|ken.js&quot;</span> <span class="token number">27</span> <span class="token number">1</span>-23/4 * * *
+
+NobyDa仓库<span class="token punctuation">(</span>拉取京东签到基础脚本JD_DailyBonus.js专用<span class="token punctuation">)</span> ql repo https://github.com/NobyDa/Script.git <span class="token string">&quot;JD-DailyBonus&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;JD_DailyBonus&quot;</span> <span class="token string">&quot;master&quot;</span> <span class="token number">29</span> <span class="token number">1</span>-23/4 * * *
+
+Tsukasa007仓库 ql repo https://github.com/Tsukasa007/my_script.git <span class="token string">&quot;jd_|jx_&quot;</span> <span class="token string">&quot;jdCookie|USER_AGENTS|sendNotify|backup&quot;</span> <span class="token string">&quot;&quot;</span> <span class="token string">&quot;master&quot;</span> <span class="token number">31</span> <span class="token number">1</span>-23/4 * * *
+
+Aaron-lv仓库 ql repo https://github.com/Aaron-lv/sync.git <span class="token string">&quot;jd_|jx_|getJDCookie&quot;</span> <span class="token string">&quot;activity|backUp|Coupon&quot;</span> <span class="token string">&quot;^jd[^_]|USER|utils&quot;</span> <span class="token string">&quot;jd_scripts&quot;</span> <span class="token number">33</span> <span class="token number">1</span>-23/4 * * *
+
+
+
+
+
+【Faker集合仓库】
+ ql repo https://ghproxy.com/https://github.com/shufflewzc/faker2.git <span class="token string">&quot;jd_|jx_|gua_|jddj_|getJDCookie&quot;</span> <span class="token string">&quot;activity|backUp&quot;</span> <span class="token string">&quot;^jd[^_]|USER|ZooFaker_Necklace.js|JDJRValidator_Pure|sign_graphics_validate&quot;</span>
+
+ 【curtinlv仓库】
+ql repo https://github.com/curtinlv/JD-Script.git
+
+【温某某】
+ql repo https://ghproxy.com/https://github.com/shufflewzc/Wenmoux.git 
+ 
+【柠檬（胖虎）】
+ql repo https://github.com/panghu999/panghu.git <span class="token string">&quot;jd_&quot;</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,10),o=[i];function p(u,l){return n(),t("div",null,o)}const d=s(e,[["render",p],["__file","repository.html.vue"]]);export{d as default};
---
+++ assets/敏感词检测API服务wordscheck.html-T2mqK1Ys.js
@@ -0,0 +1,67 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as i,c as d,a as t}from"./app-AGOZjyqV.js";const n={},c=t(`<h1 id="敏感词检测api服务wordscheck" tabindex="-1"><a class="header-anchor" href="#敏感词检测api服务wordscheck"><span>敏感词检测API服务wordscheck</span></a></h1><p><strong>什么是 wordscheck ？</strong></p><blockquote><p><code>wordscheck</code> 是敏感词检测 <code>API</code>，提供文本识别、智能鉴黄、涉政检测、谩骂等等敏感词检测过滤服务。</p></blockquote><p><strong>简介</strong></p><ul><li>敏感词库从大量样本库整理出来，基于 <code>NLP</code> 算法检测</li><li>支持 <code>Windows</code>、<code>MacOS</code>、<code>Linux</code> 等 <code>64</code> 位主流系统</li><li>可以部署在本地，或部署到阿里云、腾讯云、亚马逊云、谷歌云等云服务器</li><li>通过下载部署包，即可一键启动私有化的&quot;敏感词检测 <code>API</code> 服务&quot;</li><li>支持自动云更新最新词库</li><li>支持 <code>http json</code>方式或 <code>gRPC</code> 方式查询</li><li>单服务参考查询效率 <code>70000</code> 次/分钟，同时支持并行服务</li><li>支持自定义添加白名单/黑名单词条</li><li>服务运行内存 <code>100M</code> 左右，非常轻便</li></ul><p><strong>应用场景</strong></p><ul><li><code>AI</code> 智能问答、评论留言、聊天消息、直播弹幕、商品详情 等内容合规检测过滤</li><li>应用提审上架、主管部门审核、云平台内容巡查 等监管需要</li><li>境内外 产品内容合规需要，可部署到中国香港、新加坡、日本、美国、韩国等</li></ul><h1 id="构建镜像" tabindex="-1"><a class="header-anchor" href="#构建镜像"><span>构建镜像</span></a></h1><blockquote><p>如果你不想自己构建，可以跳过，直接阅读下一章节</p></blockquote><p>官方提供了 <code>Dockerfile</code> 文件，但没找到镜像，所以需要编自己编一下</p><p>构建镜像和容器运行的基本命令如下👇</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code># 下载代码
+git clone https://github.com/bosnzt/wordscheck.git
+  
+# 或者加个代理
+git clone https://ghproxy.com/github.com/bosnzt/wordscheck.git
+  
+# 进入目录  
+cd wordscheck
+  
+# 构建镜像
+docker build -t wbsu2003/wordscheck:v1 .
+
+# 运行容器
+docker run -d \\
+   --name wordscheck \\
+   -p 8187:8080 \\
+   wbsu2003/wordscheck:v1
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h1 id="安装" tabindex="-1"><a class="header-anchor" href="#安装"><span>安装</span></a></h1><p>在群晖上以 Docker 方式安装。</p><p>在注册表中搜索 <code>wordscheck</code> ，选择第一个 <code>wbsu2003/wordscheck</code>，版本选择 <code>latest</code>。</p><figure><img src="https://static.xlc520.tk/blogImage/640-1700829921250-0.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><h2 id="端口" tabindex="-1"><a class="header-anchor" href="#端口"><span>端口</span></a></h2><p>本地端口不冲突就行，不确定的话可以用命令查一下</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code># 查看端口占用
+netstat -tunlp | grep 端口号
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><table><thead><tr><th style="text-align:center;">本地端口</th><th style="text-align:center;">容器端口</th></tr></thead><tbody><tr><td style="text-align:center;"><code>8187</code></td><td style="text-align:center;"><code>8080</code></td></tr></tbody></table><p>默认没有暴露端口</p><figure><img src="https://static.xlc520.tk/blogImage/640-1700829921250-1.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><p>需要点 <code>+</code> 号自己添加</p><figure><img src="https://static.xlc520.tk/blogImage/640-1700829921250-2.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><h1 id="命令行安装" tabindex="-1"><a class="header-anchor" href="#命令行安装"><span>命令行安装</span></a></h1><p>如果你熟悉命令行，可能用 <code>docker cli</code> 更快捷</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code># 运行容器
+docker run -d \\
+   --restart unless-stopped \\
+   --name wordscheck \\
+   -p 8187:8080 \\
+   wbsu2003/wordscheck
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>也可以用 <code>docker-compose</code> 安装，将下面的内容保存为 <code>docker-compose.yml</code> 文件</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>version: &#39;3&#39;
+
+services:
+  wordscheck:
+    image: wbsu2003/wordscheck
+    container_name: wordscheck
+    restart: unless-stopped
+    ports:
+      - 8187:8080
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>然后执行下面的命令</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code># 新建文件夹 wordscheck
+mkdir -p /volume1/docker/wordscheck
+
+# 进入 wordscheck 目录
+cd /volume1/docker/wordscheck
+
+# 将 docker-compose.yml 放入当前目录
+
+# 一键启动
+docker-compose up -d
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h1 id="运行" tabindex="-1"><a class="header-anchor" href="#运行"><span>运行</span></a></h1><p>容器启动后，在日志中会看到</p><figure><img src="https://static.xlc520.tk/blogImage/640-1700829921250-3.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><p>但是如果你直接在浏览器中输入 <code>http://群晖IP:8187</code> ，会看到下面的错误</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>404 page not found
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><figure><img src="https://static.xlc520.tk/blogImage/640-1700829921250-4.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><p>需要通过 <code>curl</code> 来验证，用 <code>SSH</code> 客户端登录到群晖后，在命令行执行</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>curl http://192.168.0.197:8187/health
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>如果状态正常，会返回</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>{&quot;code&quot;:&quot;0&quot;}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><figure><img src="https://static.xlc520.tk/blogImage/640-1700829921250-5.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><p>测试下敏感词服务</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>curl -H &quot;Accept: application/json&quot; \\
+     -H &quot;Content-type: application/json&quot; \\
+     -X POST \\
+     -d &quot;{\\&quot;content\\&quot;:\\&quot;他在传播艳情内容\\&quot;}&quot;  \\
+     http://192.168.0.197:8187/wordscheck
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><figure><img src="https://static.xlc520.tk/blogImage/640-1700829921250-6.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><p>在输出格式化之后是👇下面这样的</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>{  
+    &quot;code&quot;:&quot;0&quot;,  
+    &quot;msg&quot;:&quot;检测成功&quot;,  
+    &quot;return_str&quot;:&quot;他在传播**内容&quot;,  
+    &quot;word_list&quot;:[  
+        {  
+            &quot;keyword&quot;:&quot;艳情&quot;,  
+            &quot;category&quot;:&quot;色情&quot;,  
+            &quot;position&quot;:&quot;4-5&quot;,  
+            &quot;level&quot;:&quot;高&quot;  
+        }  
+    ],  
+    &quot;extstr&quot;:&quot;&quot;  
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>如果觉得 <code>curl</code> 不方便，可以试试 <code>api</code> 工具</p><figure><img src="https://static.xlc520.tk/blogImage/640-1700829921251-7.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><p>官方提供了 <code>http</code> 和 <code>rpc</code> 的不同语言的调用示例，需要自己根据需要进行选择</p><figure><img src="https://static.xlc520.tk/blogImage/640-1700829921251-8.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><h1 id="参考文档" tabindex="-1"><a class="header-anchor" href="#参考文档"><span>参考文档</span></a></h1><blockquote><p>bosnzt/wordscheck: 敏感词检测，违禁词过滤，敏感词过滤，敏感词库，一键启动，本地运行，私有化部署，1分钟接入完成，支持docker，支持在线api 地址：https://github.com/bosnzt/wordscheck</p><p>介绍 - Powered by MinDoc 地址：https://doc.wordscheck.com/docs/docs</p><p>文本识别_智能鉴黄_敏感词过滤_涉政检测_在线体验_wordscheck 地址：https://www.wordscheck.com/</p></blockquote>`,53),a=[c];function s(l,o){return i(),d("div",null,a)}const v=e(n,[["render",s],["__file","敏感词检测API服务wordscheck.html.vue"]]);export{v as default};
